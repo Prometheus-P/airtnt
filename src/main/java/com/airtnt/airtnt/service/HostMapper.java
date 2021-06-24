85f595736931dc2ccf9e29dc860a650820f82ab7
Ha Seong Park <parkdavid31@gmail.com>
2021-06-24 17:58:28 +0900

    210624

--- src/main/java/com/airtnt/airtnt/service/HostMapper.java
+++ src/main/java/com/airtnt/airtnt/service/HostMapper.java
@@ -1,8 +1,18 @@
 package com.airtnt.airtnt.service;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.airtnt.airtnt.model.PropertyDTO;
 
 @Service // 
 public class HostMapper {
+	@Autowired
+	private SqlSession sqlSession;
 	
+	public int insertProperty(PropertyDTO dto) {
+		int res = sqlSession.insert("insertProperty", dto);
+		return res;
+	}
 }
